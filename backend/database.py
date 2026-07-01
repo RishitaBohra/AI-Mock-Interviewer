309b83f603fa710d4e31308106bb52cadfbf020f
Rishita Bohra <rishitabohra1575@gmail.com>
2026-07-01 20:25:54 +0530

    Set up MongoDB connection for authentication

---
+++ backend/database.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+client = MongoClient(MONGO_URI)
+
+db = client["ai_mock_interviewer"]
+
+users_collection = db["users"]
+interviews_collection = db["interviews"]
