98caeee2433d62dd251651eaccbd28c2cfb3aa66
Rishita Bohra <rishitabohra1575@gmail.com>
2026-07-12 21:57:27 +0530

    Add JWT token generation support

--- backend/auth.py
+++ backend/auth.py
@@ -1,10 +1,17 @@
 from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 pwd_context = CryptContext(
     schemes=["bcrypt"],
     deprecated="auto"
 )
-
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 def hash_password(password: str):
     return pwd_context.hash(password)
@@ -18,3 +25,23 @@ def verify_password(
         plain_password,
         hashed_password
     )
+
+def create_access_token(data: dict):
+
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({
+        "exp": expire
+    })
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+    return encoded_jwt
